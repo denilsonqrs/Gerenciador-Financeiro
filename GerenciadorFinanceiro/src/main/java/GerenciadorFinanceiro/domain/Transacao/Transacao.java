288f4687e8f7b20eeed92df5d59e3b3ceaff3ed4
denilsonqrs <denilson.quaresma.dev@gmail.com>
2025-12-01 20:39:06 -0300

    Adicao dos atributos date e time e metodos getDateTime e getMonth.

--- GerenciadorFinanceiro/src/main/java/GerenciadorFinanceiro/domain/Transacao/Transacao.java
+++ GerenciadorFinanceiro/src/main/java/GerenciadorFinanceiro/domain/Transacao/Transacao.java
@@ -1,35 +1,55 @@
 package GerenciadorFinanceiro.domain.Transacao;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Transacao {
     private String name;
     private BigDecimal value;
     private int parcel;
     private String description;
+    private LocalDate date;
+    private LocalTime time;
 
-    public Transacao(String name, BigDecimal value, String description, int parcel){
+    public Transacao(String name, BigDecimal value, LocalTime time, LocalDate date, String description, int parcel){
         this.name = name;
         this.value = value;
         this.description = description;
         this.parcel = parcel;
+        this.date = date;
+        this.time = time;
     }
-    public Transacao(String name, BigDecimal value, String description){
+    public Transacao(String name, BigDecimal value, LocalTime time, LocalDate date, String description){
         this.name = name;
         this.value = value;
         this.description = description;
+        this.date = date;
+        this.time = time;
     }
-    public Transacao(String name, BigDecimal value, int parcel){
+    public Transacao(String name, BigDecimal value, LocalTime time, LocalDate date, int parcel){
         this.name = name;
         this.value = value;
         this.parcel = parcel;
+        this.date = date;
+        this.time = time;
     }
-    public Transacao(String name, BigDecimal value){
+    public Transacao(String name, BigDecimal value, LocalTime time, LocalDate date){
         this.name = name;
         this.value = value;
+        this.date = date;
+        this.time = time;
     }
     public BigDecimal getValue(){
         return value;
+    }
+    public int getMonth(){
+        return date.getMonthValue();
+    }
+    public LocalDateTime getDateTime(){
+        LocalDateTime dateTime = LocalDateTime.of(date, time);
+        return dateTime;
     }
 
 }
