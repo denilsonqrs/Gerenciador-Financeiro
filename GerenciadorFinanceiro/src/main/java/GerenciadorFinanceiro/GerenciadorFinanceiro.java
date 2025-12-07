dda22fcb5c6fab7b618cfc3643966f342d39ca76
denilsonqrs <denilson.quaresma.dev@gmail.com>
2025-12-07 11:06:24 -0300

    Categorias base implementado, implementandon metodo de registro da transacao na categoria

--- GerenciadorFinanceiro/src/main/java/GerenciadorFinanceiro/GerenciadorFinanceiro.java
+++ GerenciadorFinanceiro/src/main/java/GerenciadorFinanceiro/GerenciadorFinanceiro.java
@@ -1,17 +1,28 @@
 package GerenciadorFinanceiro;
 
 import GerenciadorFinanceiro.domain.Categoria.Categoria;
+import GerenciadorFinanceiro.domain.Categoria.TransactionType;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class GerenciadorFinanceiro {
     List<Categoria> categorias;
     public GerenciadorFinanceiro(){
         this.categorias = new ArrayList<>();
+        initBaseCategories();
 
+    }
+    private void initBaseCategories() {
+        if (this.categorias.isEmpty()){
+            this.categorias.add(new Categoria("Food", TransactionType.EXPENSE));
+            this.categorias.add(new Categoria("Entertainment", TransactionType.EXPENSE));
+            this.categorias.add(new Categoria("Salary", TransactionType.INCOME));
+            this.categorias.add(new Categoria("Investments", TransactionType.INCOME));
+        }
     }
     public boolean categoryExist(String categoryName){
         for (Categoria categoria : categorias){
@@ -20,6 +31,18 @@ public class GerenciadorFinanceiro {
             }
         }
         return false;
+
+    }
+    public void registerTransaction(String name, BigDecimal value, LocalDate date, LocalTime time, String categoryName, String description, int installments){
+
+    }
+    public void registerTransaction(String name, BigDecimal value, LocalDate date, LocalTime time, String categoryName, String description){
+
+    }
+    public void registerTransaction(String name, BigDecimal value, LocalDate date, LocalTime time, String categoryName, int installments){
+
+    }
+    public void registerTransaction(String name, BigDecimal value, LocalDate date, LocalTime time, String categoryName){
 
     }
     public List<Categoria> getCategories(){
