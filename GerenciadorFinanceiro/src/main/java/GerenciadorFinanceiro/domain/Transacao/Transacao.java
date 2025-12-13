e213187f18500c588af39629367eabfda7f01d2a
denilsonqrs <denilson.quaresma.dev@gmail.com>
2025-12-13 12:18:26 -0300

    atributo de descrição removido.

--- GerenciadorFinanceiro/src/main/java/GerenciadorFinanceiro/domain/Transacao/Transacao.java
+++ GerenciadorFinanceiro/src/main/java/GerenciadorFinanceiro/domain/Transacao/Transacao.java
@@ -8,32 +8,15 @@ import java.time.LocalTime;
 public class Transacao {
     private String name;
     private BigDecimal value;
-    private int parcel;
+    private int installments;
     private String description;
     private LocalDate date;
     private LocalTime time;
 
-    public Transacao(String name, BigDecimal value, LocalTime time, LocalDate date, String description, int parcel) {
+    public Transacao(String name, BigDecimal value, LocalTime time, LocalDate date, int installments) {
         this.name = name;
         this.value = value;
-        this.description = description;
-        this.parcel = parcel;
-        this.date = date;
-        this.time = time;
-    }
-
-    public Transacao(String name, BigDecimal value, LocalTime time, LocalDate date, String description) {
-        this.name = name;
-        this.value = value;
-        this.description = description;
-        this.date = date;
-        this.time = time;
-    }
-
-    public Transacao(String name, BigDecimal value, LocalTime time, LocalDate date, int parcel) {
-        this.name = name;
-        this.value = value;
-        this.parcel = parcel;
+        this.installments = installments;
         this.date = date;
         this.time = time;
     }
